4a33bda34fed6188041dfb693e82863390f91919
Wanderson Rossini <wandersonrossini@gmail.com>
2026-08-14 05:15:48 +0200

    fix: add missing dart:async import for Timer in login_page

--- 6_APP_LAUSANNE_TESOURARIA/tesouraria_cme_app/lib/presentation/pages/login_page.dart
+++ 6_APP_LAUSANNE_TESOURARIA/tesouraria_cme_app/lib/presentation/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:image_picker/image_picker.dart';
