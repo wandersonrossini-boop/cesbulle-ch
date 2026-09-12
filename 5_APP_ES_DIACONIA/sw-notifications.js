789a239df823c8a6ba6073fc23260668bc5036b6
Wanderson Rossini <wandersonrossini@gmail.com>
2026-09-12 13:27:09 +0200

    fix: invalida cache do Service Worker para forçar o client a baixar o novo index.html com a correção dos modais

--- 5_APP_ES_DIACONIA/sw-notifications.js
+++ 5_APP_ES_DIACONIA/sw-notifications.js
@@ -16,8 +16,8 @@ const firebaseConfigProd = {
 firebase.initializeApp(firebaseConfigProd);
 const messaging = firebase.messaging();
 
-const CACHE_NAME = 'diaconia-cache-v3.11.8-PWA';
-const SW_VERSION = 'v3.11.8-PWA';
+const CACHE_NAME = 'diaconia-cache-v3.11.9-PWA';
+const SW_VERSION = 'v3.11.9-PWA';
 const APP_URL = '/';
 
 // App Shell: Recursos vitais a serem pré-cacheados na instalação
